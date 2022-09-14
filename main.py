7a3483f35d5b7b205957f49f211d882b9ec84a39
caiquilipe <caiqui@gdstec.com.br>
2022-09-14 10:40:49 -0300

    removed main.py

--- main.py
+++
@@ -1,5 +0,0 @@
-from pagarme_integration.payment_gateway import PaymentGatewayClass
-
-gateway = PaymentGatewayClass(key="sk_test_gj6KDaeiQVIPwN0X")
-print(gateway.get_customers())
-gateway.get_orders(customer_id="")
